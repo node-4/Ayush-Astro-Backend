d8eade98fe28b053c36cedd6d548a07684bc7630
rubiyadav <rubiflyeis@gmail.com>
2023-05-31 11:31:12 +0530

    update

--- controllers/notificationCronjob.js
+++ controllers/notificationCronjob.js
@@ -1,11 +1,32 @@
 const cronJob = require("cron").CronJob;
 let Notification = require("../models/Notification");
 let wallet = require("../models/wallet");
+let userModel = require("../models/User");
 // new cronJob('0 */2 * * *', async function () {
-new cronJob("*/5 * * * *", async function () {
-    let obj = {
-        user: "",
-        message: "",
-    };
+new cronJob("0 0 * * *", async function () {
+    let users = await userModel.find({});
+    if (users.length > 0) {
+        for (let i = 0; i < users.length; i++) {
+            let findWallet = await wallet.findOne({ user: users[i]._id });
+            if (findWallet) {
+                if (findWallet.balance < 51) {
+                    let obj = {
+                        user: users[i]._id,
+                        message:
+                            "Add amount in your wallet, your wallet balance is less than minimum balance.",
+                    };
+                    const Data = await Notification.create(obj);
+                }
+            } else {
+                let obj = {
+                    user: users[i]._id,
+                    message: "Create and add amount in your wallet.",
+                };
+                const Data = await Notification.create(obj);
+            }
+        }
+    } else {
+        console.log("no user found.");
+    }
 }).start();
 // }).stop()
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(compression({ threshold: 500 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+require('./controllers/notificationCronjob')
 if (process.env.NODE_ENV == "production") {
     console.log = function () { };
 }
