3f51482622a1c3b0693eb94e950b7edd4a2cd23a
rubiyadav <rubiflyeis@gmail.com>
2023-07-31 16:15:16 +0530

    create an api

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -3,113 +3,167 @@ const user = require('../models/User');
 const astro = require('../models/astrologer')
 
 
-exports.CreateOrder = async(req,res) => {
-    try{
-    const userId  = req.body.userId;
-    const astroId  = req.body.astroId;
-    const astroData = await astro.findById({_id: astroId});
-    const userData = await user.findById({_id: userId});
-    console.log(astroData)
-    const data = {
-        astroName: astroData.firstName,
-        user: userId,
-        astroId: req.body.astroId, 
-        time: req.body.time, 
-        name: userData.firstName + " " + userData.lastName,
-        problem : req.body.problem, 
-        language: userData.language, 
-        rashi : userData.rashi
+exports.CreateOrder = async (req, res) => {
+    try {
+        const userId = req.body.userId;
+        const astroId = req.body.astroId;
+        const astroData = await astro.findById({ _id: astroId });
+        const userData = await user.findById({ _id: userId });
+        console.log(astroData)
+        const data = {
+            astroName: astroData.firstName,
+            user: userId,
+            astroId: req.body.astroId,
+            time: req.body.time,
+            name: userData.firstName + " " + userData.lastName,
+            problem: req.body.problem,
+            language: userData.language,
+            rashi: userData.rashi
+        }
+        console.log(data)
+        const orderData = await order.create(data);
+        console.log(orderData);
+        res.status(200).json({
+            details: orderData
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({ message: err.message })
     }
-  console.log(data)
-  const orderData = await order.create(data);
-  console.log(orderData);
-  res.status(200).json({
-    details: orderData
-  })
-}catch(err){
-    console.log(err)
-    res.status(400).json({message: err.message})
-}
 
 }
 
 
-exports.GetAllOrder = async(req,res) => {
-    try{
-   const orderData = await order.find();
-   res.status(200).json({
-    details : orderData
-   })
-    }catch(err){
+exports.GetAllOrder = async (req, res) => {
+    try {
+        const orderData = await order.find();
+        res.status(200).json({
+            details: orderData
+        })
+    } catch (err) {
         console.log(err);
         res.status(400).json({
             message: err.message
         })
     }
 }
+exports.GetByOrderUserId = async (req, res) => {
+    try {
+        const { fromDate, toDate, page, limit } = req.query;
+        let query = { user: req.params.id };
+        if (fromDate && !toDate) {
+            query.createdAt = { $gte: fromDate };
+        }
+        if (!fromDate && toDate) {
+            query.createdAt = { $lte: toDate };
+        }
+        if (fromDate && toDate) {
+            query.$and = [
+                { createdAt: { $gte: fromDate } },
+                { createdAt: { $lte: toDate } },
+            ]
+        }
+        let options = {
+            page: Number(page),
+            limit: Number(limit) || 10,
+            sort: { createdAt: -1 },
+        };
+        let data = await order.paginate(query, options);
+        return res.status(200).json({ status: 200, message: "data found.", details: data });
 
-exports.GetByOrderUserId = async(req,res) => {
-    try{
-  const data = await order.find({user:req.params.id})
-  res.status(200).json({
-    details : data
-  })
-    }catch(err){
+    } catch (err) {
+        return res.status(500).send({ msg: "internal server error ", error: err.message, });
+    }
+};
+
+// exports.GetByOrderUserId = async (req, res) => {
+//     try {
+//         const data = await order.find({ user: req.params.id })
+//         res.status(200).json({
+//             details: data
+//         })
+//     } catch (err) {
+//         res.status(400).json({
+//             message: err.message
+//         })
+//     }
+// }
+
+exports.GetVyID = async (req, res) => {
+    try {
+        const data = await order.findById({ _id: req.params.id });
+        res.status(200).json({
+            details: data
+        })
+    } catch (err) {
         res.status(400).json({
             message: err.message
         })
     }
 }
 
-exports.GetVyID = async(req,res) => {
-    try{
-    const data = await order.findById({_id: req.params.id});
-    res.status(200).json({
-        details : data
-    })
-    }catch(err){
-        res.status(400).json({
-            message: err.message
-        })
-    }
-}
 
-
-exports.updateOrder = async(req,res) => {
-    try{
-    await order.findByIdAndUpdate({_id: req.params.id}, {
-        time: req.body.time, 
-        problem : req.body.problem, 
-    });
-    res.status(200).json({message: "Order Updated"})
-    }catch(err)
-    {
-        res.status(400).json({message: err.message})
+exports.updateOrder = async (req, res) => {
+    try {
+        await order.findByIdAndUpdate({ _id: req.params.id }, {
+            time: req.body.time,
+            problem: req.body.problem,
+        });
+        res.status(200).json({ message: "Order Updated" })
+    } catch (err) {
+        res.status(400).json({ message: err.message })
     }
 }
 
 
 
-exports.DeleteOrder  = async(req,res) => {
-    try{
-    await order.findByIdAndDelete({_id: req.params.id});
-    res.status(200).json({message: "Deleted  Updated"})
-    }catch(err)
-    {
-        res.status(400).json({message: err.message})
+exports.DeleteOrder = async (req, res) => {
+    try {
+        await order.findByIdAndDelete({ _id: req.params.id });
+        res.status(200).json({ message: "Deleted  Updated" })
+    } catch (err) {
+        res.status(400).json({ message: err.message })
     }
 }
 
+exports.GetByAstroId = async (req, res) => {
+    try {
+        const { fromDate, toDate, page, limit } = req.query;
+        let query = { astroId: req.params.id };
+        if (fromDate && !toDate) {
+            query.createdAt = { $gte: fromDate };
+        }
+        if (!fromDate && toDate) {
+            query.createdAt = { $lte: toDate };
+        }
+        if (fromDate && toDate) {
+            query.$and = [
+                { createdAt: { $gte: fromDate } },
+                { createdAt: { $lte: toDate } },
+            ]
+        }
+        let options = {
+            page: Number(page),
+            limit: Number(limit) || 10,
+            sort: { createdAt: -1 },
+        };
+        let data = await order.paginate(query, options);
+        return res.status(200).json({ status: 200, message: "astrologer data found.", details: data });
 
-exports.GetByAstroId = async(req,res) => {
-    try{
-    const data = await order.find({astroId: req.params.id}).sort({timestamp : -1})
-    res.status(200).json({
-        details : data
-    })
-    }catch(err){
-        res.status(400).json({
-            message: err.message
-        })
+    } catch (err) {
+        return res.status(500).send({ msg: "internal server error ", error: err.message, });
     }
-}
+};
+
+// exports.GetByAstroId = async (req, res) => {
+//     try {
+//         const data = await order.find({ astroId: req.params.id }).sort({ timestamp: -1 })
+//         res.status(200).json({
+//             details: data
+//         })
+//     } catch (err) {
+//         res.status(400).json({
+//             message: err.message
+//         })
+//     }
+// }
--- models/order.js
+++ models/order.js
@@ -1,36 +1,37 @@
 const mongoose = require('mongoose');
-
-
+const mongoosePaginate = require("mongoose-paginate");
+const mongooseAggregatePaginate = require("mongoose-aggregate-paginate");
 const OrderSchema = mongoose.Schema({
     user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true
-          },
-    astroId : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    astroId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "astrologer",
         required: true
-    }, 
+    },
     time: {
-        type: String, 
+        type: String,
     },
     // createdAt : new Date().toISOString(),
     name: {
         type: String
-    }, 
-    problem : {
+    },
+    problem: {
         type: String
-    }, 
-    language: [], 
+    },
+    language: [],
     rashi: {
         type: String
-    }, 
+    },
     astroName: {
         type: String
     }
-}, {timestamp : true})
+}, { timestamp: true })
 
+OrderSchema.plugin(mongooseAggregatePaginate);
+OrderSchema.plugin(mongoosePaginate);
 const order = mongoose.model('order', OrderSchema);
-
 module.exports = order;
